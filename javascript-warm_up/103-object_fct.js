59510e9a0599e6896f2ca4cfe8003554376d07eb
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-16 15:10:28 +0100

    adds task 8.2

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,15 +1,15 @@
 const myObject = {
     type: 'object',
     value: 12
-};
-console.log(myObject);
-myObject.incr = function () {
-    return this.value++;
-}
+    };
+    console.log(myObject);
+    myObject.incr = function () {
+        return this.value++;
+    }
 
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
+    myObject.incr();
+    console.log(myObject);
+    myObject.incr();
+    console.log(myObject);
+    myObject.incr();
+    console.log(myObject);
