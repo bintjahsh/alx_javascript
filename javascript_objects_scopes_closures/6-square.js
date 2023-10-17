c46f1a324c5d5a0ab21ed0522855dcbec78531e2
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-17 15:01:30 +0100

    adds task 6.2

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,20 +1,28 @@
 #!/usr/bin/node
 const superSquare = require('./5-square');
 class Square extends superSquare {
+    constructor (size) {
+        super(size);
+    }
     charPrint(c) {
-        for (let i = 0; i < this.size; i++) {
+        for (let i = 0; i < this.width; i++) {
             let square_print = '';
-            for (let j = 0; j < this.size; j++) {
+            for (let j = 0; j < this.height; j++) {
                 if (c) {
                     square_print += c;
                 } else {
-                    square_print += 'X'
-                }
-                
+                    square_print += 'X';
+                }   
             }
             console.log(square_print)
         }
     } 
 }
+
+// const s1 = new Square(4);
+// s1.charPrint();
+
+// s1.charPrint('D');
+// console.log('Hello')
 
 module.exports = Square
