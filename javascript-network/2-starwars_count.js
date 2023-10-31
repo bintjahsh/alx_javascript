ee1f9f6f28a3f49da0866931d470db07362d7bfc
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-10-31 23:32:09 +0000

    adds task 2.2

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -4,15 +4,14 @@ const url = process.argv[2]
 const id = "https://swapi-api.alx-tools.com/api/people/18/"
 
 request(url, function(error, response, body) {
+    let count = 0
     if (response) {
         const responseJSON = JSON.parse(response.body);
-        let count = 0
         const results = responseJSON.results
         results.forEach(film => {
             const wedgeAntilles = film.characters.filter((character) => character == id)
             count += wedgeAntilles.length
         });
-        
-        console.log(count);
     }
-})
+    console.log(count);
+});
